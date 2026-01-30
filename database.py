bd81d5703dd39c1ed880f1a8b5c847e62ddcc8c5
Aanya Sharma <128315909+Aanyaa26@users.noreply.github.com>
2026-01-30 16:31:18 +0530

    Locally deployed on Docker Container and FastAPI improvements along with debugging

--- database.py
+++ database.py
@@ -1,9 +1,12 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///./test.db"
+password = os.getenv("MYSQL_ROOT_PASSWORD", "root123")
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+DATABASE_URL = f"mysql+pymysql://root:{password}@mysql/temp"
+
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(bind=engine)
 
--- main.py
+++ main.py
@@ -1,31 +1,31 @@
-from fastapi import FastAPI, Depends
-from sqlalchemy.orm import Session
-from database import SessionLocal, engine, Base
-from models import AddressDB
-from pydantic import BaseModel, Field
+from fastapi import FastAPI, Depends # import - depends helps to inject database connection
+from sqlalchemy.orm import Session   # session represents the db connection
+from database import SessionLocal, engine, Base  # engine creates connection to MySQL , session local creates DB session , Base creates base class for DB tables (all imported from database.py)
+from models import AddressDB # imports SQLAlchemy model 
+from pydantic import BaseModel, Field # for validation
 
-app = FastAPI()
+app = FastAPI()  # creating the fast api application
 
-Base.metadata.create_all(bind=engine)
+Base.metadata.create_all(bind=engine) # if table isn't there, create one
 
-class Address(BaseModel):
+class Address(BaseModel): # defines the structure of the icoming payload
     hn: int
     ap: str
     pincode: int = Field(..., ge=100000, le=999999)
 
-class Payload(BaseModel):
+class Payload(BaseModel):  # this ensures that the payload must look like this
     address: Address
 
-def get_db():
-    db = SessionLocal()
+def get_db():  
+    db = SessionLocal()   #It creates db connection and close it safely
     try:
-        yield db
+        yield db  #gives DB connection to FastApi
     finally:
-        db.close()
+        db.close() # closes after use
 
-@app.post("/address")
+@app.post("/address")  #endpoint
 def create_address(payload: Payload, db: Session = Depends(get_db)):
-    addr = payload.address
+    addr = payload.address #extracting as an object (deserialization)
 
     db_address = AddressDB(
         hn=addr.hn,
@@ -33,7 +33,7 @@ def create_address(payload: Payload, db: Session = Depends(get_db)):
         pincode=addr.pincode
     )
 
-    db.add(db_address)
+    db.add(db_address) #saving data into the table
     db.commit()
     db.refresh(db_address)
 
